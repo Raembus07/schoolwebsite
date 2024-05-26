b45149fcc2d7ce44966ff38311d750b4bbab039f
Josia Schweizer <josia.schweizer@gmail.com>
2024-05-26 18:27:49 +0200

    updated img and added js

---
+++ src/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyImages = [].slice.call(document.querySelectorAll("img[data-high-res]"));
+
+    if ("IntersectionObserver" in window) {
+        var lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    var lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.highRes;
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+        lazyImages.forEach(function(lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    }
+});
