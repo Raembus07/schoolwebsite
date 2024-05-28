ff0ebf8728abf5b3c350b9fa3a3d4218e8f82fc0
Josia Schweizer <josia.schweizer@gmail.com>
2024-05-28 09:27:06 +0200

    updated the lowqual of video on index.html and added a highqual video

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ image1.src = '/img/highqual/index01.jpg'
 
 let image2 = document.getElementById('image2');
 image2.src = '/img/highqual/index02.jpg'
+
+let video1 = document.getElementById('video1');
+video1.src = '/vid/highqual/pizza.mp4'
