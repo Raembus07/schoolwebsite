028e5fa6b1465508be1357b85ff776f9a447348d
Josia Schweizer <josia.schweizer@gmail.com>
2024-05-28 09:16:01 +0200

    changed js for loading pictures

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyImages = [].slice.call(document.querySelectorAll("img[data-high-res]"));
+let image1 = document.getElementById('image1');
+image1.src = '/img/highqual/index01.jpg'
 
-    if ("IntersectionObserver" in window) {
-        var lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-            entries.forEach(function(entry) {
-                if (entry.isIntersecting) {
-                    var lazyImage = entry.target;
-                    lazyImage.src = lazyImage.dataset.highRes;
-                    lazyImage.classList.remove("lazy");
-                    lazyImageObserver.unobserve(lazyImage);
-                }
-            });
-        });
+let image2 = document.getElementById('image2');
+image2.src = '/img/highqual/index02.jpg'
 
-        lazyImages.forEach(function(lazyImage) {
-            lazyImageObserver.observe(lazyImage);
-        });
-    } else {
-        lazyImages.forEach(function(lazyImage) {
-            lazyImage.src = lazyImage.dataset.highRes;
-            lazyImage.classList.remove("lazy");
-        });
-    }
-});
+let image3 = document.getElementById('image3');
+image3.src = '/img/highqual/josia.jpg'
+
+let image4 = document.getElementById('image4');
+image4.src = '/img/highqual/fussball.jpg'
+
+let image5 = document.getElementById('image5');
+image5.src = '/img/highqual/volleyball.jpg'
+
+let image6 = document.getElementById('image6');
+image6.src = '/img/highqual/badminton.jpg'
+
+let image7 = document.getElementById('image7');
+image7.src = '/img/highqual/JAVA.jpg'
+
+let image8 = document.getElementById('image8');
+image8.src = '/img/highqual/c.jpg'
+
+let image9 = document.getElementById('image9');
+image9.src = '/img/highqual/sql.jpg'
